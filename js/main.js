b5bb2aff45b920164bc9b0d19ba255b0d4025400
Fernando Silva <fernando.jmelo39@gmail.com>
2025-05-14 08:06:48 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,3 +6,14 @@ function muda_cor(){
 
     body.style.backgroundColor = cores[x];
 }
+
+function muda_cor_hex(){
+    let body = document.getElementById("bg");
+    const hex = "0123456789ABCDEF";
+    let cor = "#";
+
+    for(let i=0; i<6; i++){
+        cor += hex.charAt(Math.floor(Math.random() * hex.length))
+    }
+    body.style.backgroundColor = cor;
+}
